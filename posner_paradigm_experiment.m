0e438132d7215154e3bd20d5fda93f6925a64f3f
noushinquazi <noushinquazi@yahoo.com>
2020-05-17 13:46:57 -0500

    set up screen

--- posner_paradigm_experiment.m
+++ posner_paradigm_experiment.m
@@ -13,3 +13,18 @@ data = [trials zeros(size(trials, 1),1)];
 
 % shuffle trials
 data = data(randperm(size(data,1)),:);
+
+%% set up screen
+fig = fullfig;
+screen_dims = [0 sqrt(num_pos)];
+screen_grid = allcomb(0:sqrt(num_pos), 0:sqrt(num_pos));
+xlim(screen_dims);
+ylim(screen_dims);
+% text(0, 0, "H", 'FontSize', 30);
+% text(4, 0, "H", 'FontSize', 30);
+% text(4, 1, "H", 'FontSize', 30);
+
+for r = 1:size(screen_grid, 1)
+    coord = screen_grid(r,:);
+    text(coord(1), coord(2), "H", 'FontSize', 30);
+end
